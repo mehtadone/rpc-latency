42559eaedbb5dfb1cda96f36c60c19b9d0e54272
Chirag Mehta <mehtadone@gmail.com>
2023-05-17 13:18:18 +0100

    Add ping speed and getBlock

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const ethers = require('ethers');
+const ping = require('ping');
 
 const type = process.argv[2]; // "http" or "wss"
 const tries = process.argv[3];
@@ -9,61 +10,71 @@ const network = {
   name: 'homestead'
 };
 
-const results = [];
+const bearerToken = 'your-token';
 
 const transaction = {
   from: "0x0000000000000000000000000000000000000000", // Doesn't have to exist
   to: "0x000000000000000000000000000000000000dEaD" // Ethereum burn address
 };
 
+const results = [];
+
 if(type === "http") {
-  rpcUrls.forEach(rpcUrl => {
+  rpcUrls.forEach(async rpcUrl => {
     const customHttpProvider = new ethers.providers.JsonRpcProvider(rpcUrl, {
       chainId: network.chainId,
       name: network.name
     });
 
-    async function testHttpProvider() {
-      const times = [];
-      for(let i = 0; i < tries; i++) {
-        const start = Date.now();
-        const gasEstimate = await customHttpProvider.estimateGas(transaction);
-        const end = Date.now();
-        console.log(`HTTP Iteration ${i+1}, Gas estimate: ${gasEstimate.toString()}`);
-        times.push(end - start);
-      }
-      const average = times.reduce((a, b) => a + b, 0) / times.length;
-      results.push({rpcUrl, 'Average HTTP RPC latency': average});
-      console.table(results);
+    const times = [];
+    const blockTimes = [];
+    const pingResult = await ping.promise.probe(rpcUrl.split('//')[1].split(':')[0]);
+    for(let i = 0; i < tries; i++) {
+      const start = Date.now();
+      const gasEstimate = await customHttpProvider.estimateGas(transaction);
+      const end = Date.now();
+      console.log(`HTTP Iteration ${i+1}, Gas estimate: ${gasEstimate.toString()}`);
+      times.push(end - start);
+
+      const startBlock = Date.now();
+      const block = await customHttpProvider.getBlock();
+      const endBlock = Date.now();
+      console.log(`HTTP Iteration ${i+1}, Block number: ${block.number}`);
+      blockTimes.push(endBlock - startBlock);
     }
-    
-    // Test the provider
-    testHttpProvider();
+    const average = times.reduce((a, b) => a + b, 0) / times.length;
+    const averageBlock = blockTimes.reduce((a, b) => a + b, 0) / blockTimes.length;
+    results.push({rpcUrl, 'Average HTTP RPC latency for estimateGas': average, 'Ping speed': pingResult.avg, 'Average HTTP RPC latency for getBlock': averageBlock});
+    console.table(results);
   });
 }
 
 if(type === "wss") {
-  rpcUrls.forEach(rpcUrl => {
+  rpcUrls.forEach(async rpcUrl => {
     const customWebSocketProvider = new ethers.providers.WebSocketProvider(rpcUrl, {
       chainId: network.chainId,
       name: network.name
     });
 
-    async function testWsProvider() {
-      const times = [];
-      for(let i = 0; i < tries; i++) {
-        const start = Date.now();
-        const gasEstimate = await customWebSocketProvider.estimateGas(transaction);
-        const end = Date.now();
-        console.log(`WSS Iteration ${i+1}, Gas estimate: ${gasEstimate.toString()}`);
-        times.push(end - start);
-      }
-      const average = times.reduce((a, b) => a + b, 0) / times.length;
-      results.push({rpcUrl, 'Average WSS RPC latency': average});
-      console.table(results);
-    }
+    const times = [];
+    const blockTimes = [];
+    const pingResult = await ping.promise.probe(rpcUrl.split('//')[1].split(':')[0]);
+    for(let i = 0; i < tries; i++) {
+      const start = Date.now();
+      const gasEstimate = await customWebSocketProvider.estimateGas(transaction);
+      const end = Date.now();
+      console.log(`WSS Iteration ${i+1}, Gas estimate: ${gasEstimate.toString()}`);
+      times.push(end - start);
 
-    // Test the provider
-    testWsProvider();
+      const startBlock = Date.now();
+      const block = await customWebSocketProvider.getBlock();
+      const endBlock = Date.now();
+      console.log(`WSS Iteration ${i+1}, Block number: ${block.number}`);
+      blockTimes.push(endBlock - startBlock);
+    }
+    const average = times.reduce((a, b) => a + b, 0) / times.length;
+    const averageBlock = blockTimes.reduce((a, b) => a + b, 0) / blockTimes.length;
+    results.push({rpcUrl, 'Average WSS RPC latency for estimateGas': average, 'Ping speed': pingResult.avg, 'Average WSS RPC latency for getBlock': averageBlock});
+    console.table(results);
   });
 }
